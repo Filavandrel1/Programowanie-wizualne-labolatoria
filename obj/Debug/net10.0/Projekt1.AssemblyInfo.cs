c7515e40a89aacc43869451bf0d6dd477ab12232
Marcin Dzierla <mdzierla29@gmail.com>
2026-03-05 11:21:39 +0100

    projekt 1

--- obj/Debug/net10.0/Projekt1.AssemblyInfo.cs
+++ obj/Debug/net10.0/Projekt1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projekt1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b5c09f026a19dffc7b35f74b648817204de6554")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projekt1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projekt1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
