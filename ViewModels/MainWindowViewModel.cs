0d0f44ebdbae5d69427886b17f4894519d9c5d76
Marcin Dzierla <mdzierla29@gmail.com>
2026-03-12 10:50:22 +0100

    przygotowanie pod projekt 2

--- ViewModels/MainWindowViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Projekt1.ViewModels;
-
-public partial class MainWindowViewModel : ViewModelBase
-{
-    public string Greeting { get; } = "Cześć test test teat!";
-}
--- ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace Projekt1.ViewModels;
-
-public abstract class ViewModelBase : ObservableObject
-{
-}
